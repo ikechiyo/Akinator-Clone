dcd8b67f2e4ec3b62c120780f4abda5b3b336f2b
tigerfss <tiger.fdev@gmail.com>
2015-12-27 12:44:01 +0900

    詳細の実装、データファイルのダミー追加

--- classification.py
+++ classification.py
@@ -1,16 +1,95 @@
 # -*- coding: utf-8 -*-
+import os
+import numpy as np
+from sklearn import tree
 
 class classification:
-    """description of class"""
-  
-    def load(self):
-        pass
+    """ファイルからのデータ読み込み
+        決定木での学習
+        木構造を解析し質問、回答を返す
+    """
 
-    def answer(self, listA, listB):
-        return "A君"
-    
-    def queation(self, listA, listB):
-        return "質問1"
+    def load(self):
+        
+        #相対パス取得
+        base = os.path.dirname(os.path.abspath(__file__))
+
+        #ファイルパス取得
+        data_file = os.path.normpath(os.path.join(base, './data/data.tsv'))
+        label_file = os.path.normpath(os.path.join(base, './data/label_nm.txt'))
+        feature_file = os.path.normpath(os.path.join(base, './data/feature_nm.txt'))
+        
+        #データ読み込み
+        self.data = np.loadtxt(data_file, delimiter='\t')
+        self.label = open(label_file, 'r', encoding='utf-8').readlines()
+        for i in range(len(self.label)):
+            self.label[i] = self.label[i].rstrip("\n")
+        self.feature = open(feature_file, 'r', encoding='utf-8').readlines()
+        for i in range(len(self.feature)):
+            self.feature[i] = self.feature[i].rstrip("\n")
+
+        #学習
+        self.clf = tree.DecisionTreeClassifier()
+        self.clf.fit(self.data, self.label)
+
+    def answer(self, feature_nm_list, data_list):
+        
+        feature_list = self.__get_feature_list(feature_nm_list)
+        queation, answer = self.__recursion(0, 0, feature_list, data_list)
+
+        return answer
+
+    def queation(self, feature_nm_list, data_list):
+                
+        feature_list = self.__get_feature_list(feature_nm_list)
+        queation, answer = self.__recursion(0, 0, feature_list, data_list)
+
+        return queation
+
+    def __get_feature_list(self, feature_nm_list):
+        #特徴名からindex番号のリストを取得
+        feature_list = []
+        for feature_nm in feature_nm_list:
+            if feature_nm in self.feature:
+                feature_list.append(self.feature.index(feature_nm))
+        return feature_list
+
+    def __recursion(self, node_index, class_cnt, feature_list, data_list):         
+        #tree構造のレンジ外なら(ERROR)
+        if self.clf.tree_.node_count <= node_index:
+            raise "tree構造のレンジ外"
+
+        feature_index = self.clf.tree_.feature[node_index]
+  
+        #特徴が-2の場合終端ノードなので、labelを返す
+        if feature_index == "-2":
+            value_list = self.clf.tree_.value[node_index]
+            label_index = value_list.index(max(value_list))
+            return "", self.label[label_index]
+
+        #現在の階層より、取得済みの回答が少ない場合、問題を返す
+        if class_cnt > len(feature_list) - 1:
+            return self.feature[feature_index], ""
+            
+        print(feature_list[class_cnt], feature_index)
+        #treeの特徴と回答の特徴が一致しない場合(ERROR)
+        if feature_list[class_cnt] != feature_index:
+            raise "treeの特徴と回答の特徴が一致しない"
+
+        feature_threshold = self.clf.tree_.threshold[node_index]
+        
+        #境界値以下の場合は左のノード、それ以外は右のノード
+        if data_list[class_cnt] <= feature_threshold:
+            next_index = self.clf.tree_.children_left[node_index]
+        else :
+            next_index = self.clf.tree_.children_right[node_index]
+
+        return self.__recursion(next_index, class_cnt + 1, feature_list, data_list)
+
 
 if __name__ == '__main__':
-    main()
+    clf = classification()
+    clf.load()
+    print(clf.queation([], []))
+    #print(clf.queation(["問1"], [2]))
+    
