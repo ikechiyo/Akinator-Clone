281904150c39b4a20aba9a00459cfdfed5701d5a
ryochin <ryoko@Ryoko-no-MacBook-Pro.local>
2015-12-27 12:59:29 +0900

    詳細の実装

--- akinator_clone_cl.py
+++ akinator_clone_cl.py
@@ -21,19 +21,49 @@ def main():
 
 #問題を出力
 #print(clf.queation([0, 1, 2], [1, 3, 5]))
+    while True:
+        print("ーーーーーーーーーーーーーーーーーーーー")
+        print(clf.queation([0, 1, 2], [1, 3, 5]))
+        print("￣￣￣￣￣￣￣￣￣∨￣￣￣￣￣￣￣￣￣￣")
+        print("　　　　　　　　∧_∧")
+        print("　　　　　　　 < ｀∀´>　")
+        print("　　　　　　　　(　∽) ")
+        print("　　　　　　　  )ノ")
+        print("　　　　　　　　（_　")
+        print("　　　　　　　[il=li]　")
+        print("　　　　　　　　)=(_") 
+        print("　　　　　　　(-==-)") 
+        print("　　　　　　　 `ｰ‐'' ") 
+        print("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓") 
+        print("┃ 「YES」なら 5 、「NO」なら 1	┃")
+        print("┃ 「わからない場合」は 0 を入力 ┃") 
+        print("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛") 
+
 
 #回答を受け取る
 #input()
+        str = input()
+        answer = clf.answer([0, 1, 2], [1, 3, 5])
 
 #回答があるかどうかを調べる
 #str = clf.answer([0, 1, 2], [1, 3, 5])
 #もし～ならば(if, else, elsif)
 
+        if answer == "":
+            pass
+        else:
+            print("＿人人人人人人＿") 
+            print(answer)
+            print("￣Y^Y^Y^Y^Y^Y￣") 
+            break
+
 #回答があったら出力してループを抜ける
 #print
 
+
 #回答がなかったら1に戻る
 #～のあいだ(while, else)
+
 
 #ここまで
 ##################################################
